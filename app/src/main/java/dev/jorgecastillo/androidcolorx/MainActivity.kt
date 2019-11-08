d74630d0261c61491ca14e9fb1dc25486015ac21
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-11-08 21:46:31 +0100

    first commit

---
+++ app/src/main/java/dev/jorgecastillo/androidcolorx/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.jorgecastillo.androidcolorx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
