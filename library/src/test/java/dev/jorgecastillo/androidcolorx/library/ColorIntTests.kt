20fdfbb8b511088ee4a93f73f0bbac695ac2bfda
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-11-09 12:14:12 +0100

    Up to date.

--- library/src/test/java/dev/jorgecastillo/androidcolorx/library/ColorIntTests.kt
+++ library/src/test/java/dev/jorgecastillo/androidcolorx/library/ColorIntTests.kt
@@ -3,7 +3,8 @@ package dev.jorgecastillo.androidcolorx.library
 import android.graphics.Color
 import android.os.Build
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
@@ -12,9 +13,16 @@ import org.robolectric.annotation.Config
 @RunWith(AndroidJUnit4::class)
 class ColorIntTests {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `is dark should return true for #e91e63`() {
         val color = Color.parseColor("#e91e63")
-        color
+
+        assertTrue(color.isDark())
+    }
+
+    @Test
+    fun `is dark should return false for #ffffff`() {
+        val color = Color.parseColor("#ffffff")
+
+        assertFalse(color.isDark())
     }
 }
