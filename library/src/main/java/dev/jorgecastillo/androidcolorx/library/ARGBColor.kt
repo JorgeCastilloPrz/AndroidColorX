748402e2602f6bd1e11155eb43e69637fe31388c
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-11-09 09:35:41 +0100

    Add extensions to convert from argb to any other types.

--- library/src/main/java/dev/jorgecastillo/androidcolorx/library/ARGBColor.kt
+++ library/src/main/java/dev/jorgecastillo/androidcolorx/library/ARGBColor.kt
@@ -16,3 +16,14 @@ data class ARGBColor(
 
 fun @receiver:ColorInt Int.asARGB(): ARGBColor =
     ARGBColor(Color.alpha(this), Color.red(this), Color.green(this), Color.blue(this))
+
+@ColorInt
+fun ARGBColor.asColorInt(): Int = Color.argb(alpha, red, green, blue)
+
+fun ARGBColor.asRgb(): RGBColor = asColorInt().asRGB()
+
+fun ARGBColor.asCmyk(): CMYKColor = asColorInt().asCMYK()
+
+fun ARGBColor.asHex(): HEXColor = asColorInt().asHex()
+
+fun ARGBColor.asHsl(): HSLColor = asColorInt().asHSL()
